7057fe353ba6020bc2d2aa01ce5b588089ab6240
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2024-06-12 19:56:43 +0000

    datatypes

--- basics/project.js
+++ basics/project.js
@@ -1,1 +1,15 @@
-console.log("hello Harshita");
+// console.log("hello Harshita");
+const accountId=110980;
+let accMail="harshitat216@gmail.com";
+var accPassword="12345";
+accountCity="Lucknow";
+let accountPosting;
+// console.log(accMail);
+// console.log(accPassword);
+// console.log(accountId);
+console.log([accountId, accMail, accPassword]);
+console.table([accountId, accMail, accPassword,accountCity, accountPosting]);
+/* 
+prefer to use let and const only 
+*/
+console.table([typeof null, typeof undefined, typeof "Harshita", typeof 5, typeof false]);

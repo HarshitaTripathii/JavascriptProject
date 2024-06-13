3d1be97e1d38bfe82f0265a2d7a782d9499d0990
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2024-06-13 17:57:26 +0000

    comparison

--- basics/project.js
+++ basics/project.js
@@ -1,9 +1,9 @@
 // console.log("hello Harshita");
-const accountId=110980;
-let accMail="harshitat216@gmail.com";
-var accPassword="12345";
-accountCity="Lucknow";
-let accountPosting;
+// const accountId=110980;
+// let accMail="harshitat216@gmail.com";
+// var accPassword="12345";
+// accountCity="Lucknow";
+// let accountPosting;
 // console.log(accMail);
 // console.log(accPassword);
 // console.log(accountId);
@@ -15,21 +15,35 @@ prefer to use let and const only
 
 // console.table([typeof null, typeof undefined, typeof "Harshita", typeof 5, typeof false]);
 
-let comb="Ayush123";
+// let comb="Ayush123";
 // console.log(typeof comb);
 // console.log(typeof Number(comb));
 // console.log(comb);
 // let VlueInNum= Number(comb);
 // console.log(typeof(VlueInNum));
 // console.log(VlueInNum);
-let valve=true;
+// let valve=true;
 // console.log(typeof valve);
 // console.log(valve);
 
-let check1= null;
-let check2="";
-let check3= 1;
-let check4= "Harshita";
-console.table([check1, typeof check1, check2, typeof check2,check3, typeof check3, check4, typeof check4]);
-console.table([check1, Boolean(check1), check2, Boolean(check2),check3,  Boolean(check3), check4, Boolean(check4)]);
+// let check1= null;
+// let check2="";
+// let check3= 1;
+// let check4= "Harshita";
+// console.table([check1, typeof check1, check2, typeof check2,check3, typeof check3, check4, typeof check4]);
+// console.table([check1, Boolean(check1), check2, Boolean(check2),check3,  Boolean(check3), check4, Boolean(check4)]);
 
+
+// console.log(typeof("1"+2) , "1"+2 );
+// console.log(typeof(1+"2") , 1+"2" );
+// console.log(typeof("1"+2+2) , "1"+2+2 );
+// console.log(typeof(1+"2"+2) , 1+"2"+2 );
+// console.log(typeof(1+2+"2") , 1+2+"2" );
+
+// let count = 10;
+// console.log(count);
+// console.log(++count); //11
+// console.log(count);
+// console.log(count++); //12
+// console.log(count);
+// console.log(typeof True);

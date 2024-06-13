854fbdda79fda94390e5a7ccb03203db95b9b999
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2024-06-13 14:38:53 +0000

    Data conversion

--- basics/project.js
+++ basics/project.js
@@ -7,9 +7,29 @@ let accountPosting;
 // console.log(accMail);
 // console.log(accPassword);
 // console.log(accountId);
-console.log([accountId, accMail, accPassword]);
-console.table([accountId, accMail, accPassword,accountCity, accountPosting]);
+// console.log([accountId, accMail, accPassword]);
+// console.table([accountId, accMail, accPassword,accountCity, accountPosting]);
 /* 
 prefer to use let and const only 
 */
-console.table([typeof null, typeof undefined, typeof "Harshita", typeof 5, typeof false]);
+
+// console.table([typeof null, typeof undefined, typeof "Harshita", typeof 5, typeof false]);
+
+let comb="Ayush123";
+// console.log(typeof comb);
+// console.log(typeof Number(comb));
+// console.log(comb);
+// let VlueInNum= Number(comb);
+// console.log(typeof(VlueInNum));
+// console.log(VlueInNum);
+let valve=true;
+// console.log(typeof valve);
+// console.log(valve);
+
+let check1= null;
+let check2="";
+let check3= 1;
+let check4= "Harshita";
+console.table([check1, typeof check1, check2, typeof check2,check3, typeof check3, check4, typeof check4]);
+console.table([check1, Boolean(check1), check2, Boolean(check2),check3,  Boolean(check3), check4, Boolean(check4)]);
+

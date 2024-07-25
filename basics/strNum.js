49e02e76b17b341313b6e1edcfc06cef105f5a66
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2024-07-25 19:10:46 +0000

    str

---
+++ basics/strNum.js
@@ -0,0 +1,13 @@
+let name1="harshita";
+let age=21;
+// template Literal using placeholder ${}
+// console.log(`My name is ${name1} and i am ${age} years old girl.`); 
+let name2= new String('Harshit');
+// console.log(name2);
+// console.log(name2[3]);
+// console.log(name1[7]);
+console.log(name1.length);
+console.log(name1.toUpperCase());
+console.log(name1.charAt(4));
+console.log(name1.indexOf('s'));
+

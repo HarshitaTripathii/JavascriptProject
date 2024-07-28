774b095cb888549212c7f42b1bf04852d02cc03d
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2024-07-28 19:12:24 +0000

    learnArr

---
+++ basics/array.js
@@ -0,0 +1,37 @@
+const arr= [1,2,3,4,5];
+const arr2 = new Array(9,8,7,6);
+// console.log(arr);
+// const arr1= ["Yashi", 1, true, 2,3];
+// console.log(arr,arr1);
+// console.log(arr[4]);
+
+// ARRAY METHODS
+
+// console.log(arr.pop());  //returns deleted value, DELETES LAST ENTERED ITEM
+// console.log(arr);
+// console.log(arr.pop(2));  // here giving index is useless , equivalent to pop()= pop(index)
+
+// console.log(arr.push(5));
+// console.log(arr);
+// console.log(arr.push(5)); //returns length of updated array
+// console.log(arr);
+
+// console.log(arr.unshift(3));    //returns length of updated array
+// console.log(arr);
+
+// console.log(arr.shift());       //returns deleted value, DELETES FIRST ITEM
+// console.log(arr);
+
+// console.log(arr.includes(200));
+// console.log(arr.includes(2));
+
+// console.log(arr.indexOf(200));  // returns -1 if element not found
+// console.log(arr.indexOf(2));
+
+// const newAr= arr.join();  //to convert values of array into string
+
+// console.log(`${newAr} - ${typeof newAr}` );   // it is a STRING , so no need of [] brackets
+// console.log(arr);     // arr will show [] brackets
+// console.log( `${arr} - ${typeof arr}`);     // arr will not show [] brackets 
+
+

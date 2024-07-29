70906916378d88a56f57085cd26e24178023138e
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2024-07-29 11:06:07 +0000

    arrayBasics

--- basics/array.js
+++ basics/array.js
@@ -1,6 +1,6 @@
 const arr= [1,2,3,4,5];
-const arr2 = new Array(9,8,7,6);
-// console.log(arr);
+// const arr2 = new Array(9,8,7,6);
+// console.log(arr2);
 // const arr1= ["Yashi", 1, true, 2,3];
 // console.log(arr,arr1);
 // console.log(arr[4]);
@@ -13,10 +13,10 @@ const arr2 = new Array(9,8,7,6);
 
 // console.log(arr.push(5));
 // console.log(arr);
-// console.log(arr.push(5)); //returns length of updated array
+// console.log(arr.push(5)); //returns length of updated array  to add item in last
 // console.log(arr);
 
-// console.log(arr.unshift(3));    //returns length of updated array
+// console.log(arr.unshift(3));    //returns length of updated array  to add item at first place
 // console.log(arr);
 
 // console.log(arr.shift());       //returns deleted value, DELETES FIRST ITEM
@@ -28,10 +28,28 @@ const arr2 = new Array(9,8,7,6);
 // console.log(arr.indexOf(200));  // returns -1 if element not found
 // console.log(arr.indexOf(2));
 
-// const newAr= arr.join();  //to convert values of array into string
+// const newAr= arr.join();
+// console.log(newAr);   NOTE- COMMA IS ALSO A PART OF STRING
+// console.log(newAr[1]);
+
+  //to convert values of array into string
 
 // console.log(`${newAr} - ${typeof newAr}` );   // it is a STRING , so no need of [] brackets
 // console.log(arr);     // arr will show [] brackets
 // console.log( `${arr} - ${typeof arr}`);     // arr will not show [] brackets 
+
+// console.log("A ",arr);
+
+// const narr=arr.slice(1,3);  // using slice, exlcudes upper limit, do not alters original array
+// console.log("USING SLICE : ", narr);  
+
+// console.log("B ",arr);
+
+// const n1arr=arr.splice(1,3);  //using splice, inlcudes upper limit, Alters original array, remove the sliced part
+// console.log("USING SLICE : ", n1arr);
+
+// console.log("C ",arr);
+
+
 
 

1abdda402add12ae2709783c0dfadaa8be30df6c
HarshitaTripathii <harshitat216@gmail.com>
2024-10-20 15:37:15 +0530

    scopes

--- basics/function.js
+++ basics/function.js
@@ -56,23 +56,47 @@ function sum2(n1,n2)
 // console.log(TotalPrice(100,200,300,400,500,600));
 // here above, 1st argu =val1, 2nd argu =bal2, rest are combined in a list in num1
 
-const detail={
-    name:"harshita", age: 21, location:"Noida"
-};
-const array1=[24,36,48,60];
+// const detail={
+//     name:"harshita", age: 21, location:"Noida"
+// };
+// const array1=[24,36,48,60];
 
-function fetchArray(a)
+// function fetchArray(a)
+// {
+//     return a[3];
+// }
+// console.log(fetchArray(array1));
+
+// function Statement(o)
+// {
+//     return `Name of customer is ${o.name} and living in ${o.location}`;
+// };
+// console.log(Statement(detail))
+// console.log(fetchArray([10,20,30,55]));
+
+// var can be declared and can be updated
+// it works globally.
+var a=10;
+if(true)
 {
-    return a[3];
+    var a=20;
+    console.log(a);
 }
-console.log(fetchArray(array1));
+console.log(a);
 
-function Statement(o)
+
+// let cant be redeclared , but can be updated
+// const neither be updated, nor be redeclared
+// they both works within scopes
+let b=10;
+if(true)
 {
-    return `Name of customer is ${o.name} and living in ${o.location}`;
-};
-console.log(Statement(detail))
-console.log(fetchArray([10,20,30,55]));
+    let b=20;
+    console.log(b);
+}
+console.log(b);
+
+
 
 
 

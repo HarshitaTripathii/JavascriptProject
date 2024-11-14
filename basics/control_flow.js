049d44822ec228368b20269186d2631b5cf4292a
HarshitaTripathii <148742116+HarshitaTripathii@users.noreply.github.com>
2024-11-14 18:22:08 +0000

    falsy_truthy_value

--- basics/control_flow.js
+++ basics/control_flow.js
@@ -6,16 +6,29 @@
 //     else console.log(`${num} is an odd no.`);
 // })(53)
 
-let marks=10;
-switch(marks){
-    case 100:
-        console.log("pass");
-        break;
-    case 0:
-        console.log("fail");
-        break;
-    default:
-        console.log("cant be determined");
-        break;
+// let marks=10;
+// switch(marks){
+//     case 100:
+//         console.log("pass");
+//         break;
+//     case 0:
+//         console.log("fail");
+//         break;
+//     default:
+//         console.log("cant be determined");
+//         break;
+// }
 
+// truthy value
+// "false" "0" [], function(){}, {}, true, any value , " "
+
+// falsy value
+//  NaN, null, undefined, "", 0, false, -0
+
+let value="0";
+// if (value==0){
+//     console.log("false value");
+// }
+if (value==="0"){
+    console.log("true value");
 }

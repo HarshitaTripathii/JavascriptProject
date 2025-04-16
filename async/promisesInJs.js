972a347f1f76319505c5fb62e82a6a22579eac9d
HarshitaTripathii <harshitat216@gmail.com>
2025-04-16 13:59:51 +0530

    promise 2 done

--- async/promisesInJs.js
+++ async/promisesInJs.js
@@ -1,6 +1,6 @@
 const promiseOne = new Promise(function(resolve, reject){
     setTimeout(function(){
-        console.log("aysnc completed ");
+        console.log("aysnc 1 completed ");
         resolve();
         
     },1000)
@@ -13,5 +13,11 @@ promiseOne.then(function()
 })
 
 new Promise(function(resolve, reject){
-    
+    setTimeout(function(){
+        console.log("aysnc 2 completed");
+        resolve();
+    }, 1000)
+
+}).then(function(){
+    console.log("then / resolve has been called ");
 })

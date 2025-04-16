7b885e48a2f2349a4d9c22ca596242e483475e79
HarshitaTripathii <harshitat216@gmail.com>
2025-04-16 15:11:35 +0530

    catch( )

--- async/promisesInJs.js
+++ async/promisesInJs.js
@@ -1,3 +1,4 @@
+/*
 const promiseOne = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("aysnc 1 completed ");
@@ -22,6 +23,8 @@ new Promise(function(resolve, reject){
     console.log("then / resolve has been called ");
 })
 
+
+// resolve parameter is connnected/ linked to then (), you can pass, object, string anything
 new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("resolve with parameter");
@@ -29,4 +32,27 @@ new Promise(function(resolve, reject){
     },1000)
 }).then(function(user){
     console.log(user)
+})
+*/
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error=true;
+        if(!error)
+        {
+            resolve({name:"Arya", age:21});
+        }
+        else{
+            reject("ERROR : something went wrong ");
+        }
+    },1000)
+    
+    
+}).then(function(obj){
+    console.log(obj);
+    return obj.age;
+}).then(function(AgeOfuser){
+    console.log(`age is ${AgeOfuser}`);
+}).catch(function(err){
+    console.log(err);
 })

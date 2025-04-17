3391e4d4c23c8706e07be771663cd20b9cdc315a
HarshitaTripathii <harshitat216@gmail.com>
2025-04-17 23:47:45 +0530

    socket packages installation done

---
+++ SocketLearn/backend/server.js
@@ -0,0 +1,16 @@
+const app=require('express')();
+const  server=require('http').createServer(app);
+
+const io=require('socket.io')(server);
+
+io.on("connection",(socket)=>{
+    console.log("What is socket", socket);
+    console.log("Sokcet is actuve to connect");
+    socket.on("chat", (payload)=>{
+        console.log("What is payload", payload);
+        io.emit("chat",payload);
+        
+    });
+
+    
+})
